1c49c733c2eeaeabb9bf80045a9bd8bb2fd5030b
Sebastian <34491851+H0kas@users.noreply.github.com>
2018-01-30 14:29:59 +0100

    Add files via upload

---
+++ js/add_class.js
@@ -0,0 +1,9 @@
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+    document.querySelector(".hamburger").classList.toggle("cross");
+}, false);
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+    document.querySelector("nav").classList.toggle("nav-opened");
+}, false);
